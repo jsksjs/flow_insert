e50ef73dd3fba2be5b2788796e02918f8b2f4b36
Joseph Spada <jspada@cs.hartford.edu>
2018-11-02 21:46:59 -0400

    some work on inventory

--- import_fake_users.py
+++ import_fake_users.py
@@ -4,14 +4,14 @@ import csv
 
 # read cfg for host and database to connect to
 with open("db.cfg") as f:
-	host = f.readline().rstrip('\n')
-	database = f.readline().rstrip('\n')
+    host = f.readline().rstrip('\n')
+    database = f.readline().rstrip('\n')
 
 # read cfg for credentials (username and password to DB)
-	# TODO: decrypt using a key found in another file
+    # TODO: decrypt using a key found in another file
 with open("cred.cfg") as f:
-	usr = f.readline().rstrip('\n')
-	pwd = f.readline().rstrip('\n')
+    usr = f.readline().rstrip('\n')
+    pwd = f.readline().rstrip('\n')
 
 # connect
 db = con.MYSQL(host, database, usr, pwd)
@@ -26,41 +26,41 @@ query_base = "INSERT INTO user (UserID, UserName) VALUES "
 query = query_base
 
 with open('C:\\Users\\Nick\\Desktop\\Working\\Flow_Insert\\Data\\FakeUsers.csv', mode='r') as infile:
-	reader = csv.reader(infile)
-	infile.readline()
+    reader = csv.reader(infile)
+    infile.readline()
 
-	values = list(reader)
-	buffer_size = 2
-	i = 0
-	for row in values:
-		i = i + 1
-		if i >= buffer_size:
-			print('\r\ni >= buffer_size')
-			query = query[:-1] + ";"
-			print('query = ')
-			print(query)
-			print("values = ")
-			print(id)
-			r = db.query(query, id, False)
-			#print('query results below:')
-			#print(r)
-			i = 0
-			id = []
-			query = query_base
-		row[0] = None
-		print('row = %s', row)
-		query += '(%s, %s),'
-		id.extend(row)
-	if i != 0:
-		print('\r\noutside of loop:')
-		query = query[:-1] + ";"
-		#print('query results below:')
-		print('query = ')
-		print(query)
-		print("values = ")
-		print(id)
-		r = db.query(query, id, False)
-		#print(r)
+    values = list(reader)
+    buffer_size = 2
+    i = 0
+    for row in values:
+        i = i + 1
+        if i >= buffer_size:
+            print('\r\ni >= buffer_size')
+            query = query[:-1] + ";"
+            print('query = ')
+            print(query)
+            print("values = ")
+            print(id)
+            r = db.query(query, id, False)
+            #print('query results below:')
+            #print(r)
+            i = 0
+            id = []
+            query = query_base
+        row[0] = None
+        print('row = %s', row)
+        query += '(%s, %s),'
+        id.extend(row)
+    if i != 0:
+        print('\r\noutside of loop:')
+        query = query[:-1] + ";"
+        #print('query results below:')
+        print('query = ')
+        print(query)
+        print("values = ")
+        print(id)
+        r = db.query(query, id, False)
+        #print(r)
 
 """r = db.query("select * from user where UserID=%s;", [(id)], True)"""
 
